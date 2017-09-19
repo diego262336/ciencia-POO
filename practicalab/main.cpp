25807fe7ed1b8f75840055da59eda8a5484f5c2b
diego262336 <diego_26_23_36@hotmail.com>
2017-09-19 00:08:17 -0500

    Add files via upload

---
+++ practicalab/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+/*
+int suma(int arr[], int n )
+{
+    if(n==1){
+        return arr[0];
+    }else
+        return arr[n-1]+suma(arr, n-1);
+}
+int main()
+{
+
+    int lista[5]={1,2,3,4,5};
+    cout<<suma(lista,5);
+
+}*/
+/*
+int reversa(int arreg[], const int length)
+{
+    for(int i=0,j=length-1; i<j-1;i++,j--)
+    {
+        int temp=arreg[i];
+        arreg[i]=arreg[j];
+        arreg[j]=temp;
+    }
+    return 0;
+}
+
+int main()
+{
+
+    int lis[5]={12,15,26,48,96};
+    reversa(lis,5);
+    for(int i=0;i<5;i++){
+        cout<<lis[i]<<" ";
+    }
+}*/
+
+/*
+int main()
+{
+    char cad[]={"hola,\n\tapagare fb,\n\tapagare whsp,\nbye,"};
+    char cad2[]={ };
+    cout << cad;
+
+    cout<<strlen(cad);
+
+    for(int i=38;i<=38;i--){
+        if(cad[i]==','){
+            cout<<strcpy(cad,cad2);
+        }
+    }
+    return 0;
+
+    //char m[x] = ""; (x es el tamaño del arreglo)
+
+
+}*/
+
+
+
+
+
+int main()
+{
+    int matriz[1][6]={(1,2,3,4,5,6) };
+    cout<<matriz;
+}
+
+
+
+
+
+/*
+int main()
+{
+    //;
+    int arreglo[ ]={ (1,2,3),(4,5,6),(7,8,9) };
+
+    for(int i=0;i>3;i++){
+        for(int j=0;j>3;j++)
+            cout<<arreglo[i];
+
+    }
+
+}*/
+
+
+
+
+

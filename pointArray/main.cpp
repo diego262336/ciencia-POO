854e0d629b047bac5520e0d1f903c82edcaef703
diego262336 <diego_26_23_36@hotmail.com>
2017-11-16 11:32:41 -0500

    Add files via upload

---
+++ pointArray/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Point.h"
+#include "PointArray.h"
+using namespace std;
+
+int main()
+{
+
+    PointArray punto;
+
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
